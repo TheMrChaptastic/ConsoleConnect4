a965edeb3a4bfa9df1daf0069b766e7255af9690
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-22 16:09:27 -0600

    moved things to classes

---
+++ Connect4/Game.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    public class Game
+    {
+        public string[,] gameboard = new string[6, 7];
+        public bool gameOver { get; set; } = false;
+        public int turn { get; set; } = 0;
+        public int inputRow { get; set; }
+        public int turnSpotX { get; set; } = 0;
+        public int turnSpotY { get; set; } = 0;
+
+        public void PrintGameBoard()
+        {
+            Console.WriteLine("Player 1 = R | Player 2 = B");
+            Console.WriteLine("1  2  3  4  5  6  7");
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int u = 0; u < gameboard.GetLength(1); u++)
+                {
+                    Console.Write(gameboard[i, u] + "  ");
+                }
+                Console.Write("\n");
+            }
+            if (turn % 2 == 0)
+            {
+                Console.WriteLine("Player 2(Blue) Turn " + turn);
+            }
+            else
+            {
+                Console.WriteLine("Player 1(Red) Turn " + turn);
+            }
+        }
+
+        public void newGame()
+        {
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int u = 0; u < gameboard.GetLength(1); u++)
+                {
+                    gameboard[i, u] = "X";
+                }
+            }
+            Console.Clear();
+            turn = 1;
+            Console.WriteLine("New Game Created");
+            Console.WriteLine("Player 1 = R | Player 2 = B");
+            Console.WriteLine("1  2  3  4  5  6  7");
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int u = 0; u < gameboard.GetLength(1); u++)
+                {
+                    Console.Write(gameboard[i, u] + "  ");
+                }
+                Console.Write("\n");
+            }
+            if (turn % 2 == 0)
+            {
+                Console.WriteLine("Player 2(Blue) Turn " + turn);
+            }
+            else
+            {
+                Console.WriteLine("Player 1(Red) Turn " + turn);
+            }
+        }
+
+        public int playerInput()
+        {
+            Console.Write("Enter Row: ");
+            int returnLine = 0;
+            bool isNumber = false;
+            string input = Console.ReadLine();
+
+            if (input == "new game")
+            {
+                newGame();
+            }
+
+            try
+            {
+                isNumber = Int32.TryParse(input, out returnLine);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please use a number");
+            }
+
+            if (isNumber != true)
+            {
+                return playerInput();
+            }
+
+            if (returnLine >= 1 && returnLine <= 7)
+            {
+                return returnLine - 1;
+            }
+            else
+            {
+                Console.WriteLine("Please enter number -> 1-7");
+                returnLine = playerInput();
+            }
+            return returnLine;
+        }
+
+        public void updateGameboard(int x)
+        {
+            for (int i = 5; i >= 0; i--)
+            {
+                if (i == 0 && gameboard[i, x] != "X")
+                {
+                    turn--;
+                }
+
+                if (gameboard[i, x] == "X")
+                {
+                    if (turn % 2 == 0)
+                    {
+                        gameboard[i, x] = gameboard[i, x].Replace("X", "B");
+                        turnSpotX = x;
+                        turnSpotY = i;
+                    }
+                    else
+                    {
+                        gameboard[i, x] = gameboard[i, x].Replace("X", "R");
+                        turnSpotX = x;
+                        turnSpotY = i;
+                    }
+                    break;
+                }
+            }
+        }
+        public void GameOver(string c)
+        {
+            var newGamePrompt = "";
+            if (c == "draw")
+            {
+                Console.WriteLine($"Game ended in a draw...");
+                Console.WriteLine("New game: (Y, N)");
+                newGamePrompt = Console.ReadLine();
+                if (newGamePrompt.ToLower() == "y")
+                {
+                    newGame();
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            }
+            else if (c == "B")
+            {
+                Console.WriteLine($"BLUE WINS IN {turn - 1} TURNS!!!");
+                Console.WriteLine("New game: (Y, N)");
+                newGamePrompt = Console.ReadLine();
+                if (newGamePrompt.ToLower() == "y")
+                {
+                    newGame();
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"RED WINS IN {turn - 1} TURNS!!!");
+                Console.WriteLine("New game: (y, n)");
+                newGamePrompt = Console.ReadLine();
+                if (newGamePrompt.ToLower() == "y")
+                {
+                    newGame();
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            }
+        }
+    }
+}
---
+++ Connect4/GameRules.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    static class GameRules
+    {
+        public static void winChecker(Game game)
+        {
+            var gameboard = game.gameboard;
+            var cVar = "";
+            var count = 0;
+            var x = 0;
+            var y = 0;
+
+            //Check Horizontal -
+            for (int i = 0; i < gameboard.GetLength(0); i++)
+            {
+                for (int u = 0; u < gameboard.GetLength(1); u++)
+                {
+                    if (gameboard[i, u] == cVar && gameboard[i, u] != "X")
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        cVar = gameboard[i, u];
+                        count = 1;
+                    }
+
+                    if (count == 4)
+                    {
+                        //Console.WriteLine("Hor: " + u + "-" + i);
+                        game.GameOver(cVar);
+                    }
+                }
+                count = 0;
+            }
+            //Check Vertical |
+            for (int u = 0; u < gameboard.GetLength(1); u++)
+            {
+                for (int i = 0; i < gameboard.GetLength(0); i++)
+                {
+                    if (gameboard[i, u] == cVar && gameboard[i, u] != "X")
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        cVar = gameboard[i, u];
+                        count = 1;
+                    }
+
+                    if (count == 4)
+                    {
+                        //Console.WriteLine("Vert: " + u + "-" + i);
+                        game.GameOver(cVar);
+                    }
+                }
+                count = 0;
+            }
+
+            //Check Diagonal /\
+            for (int u = 0; u < 4; u++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    cVar = "X";
+                    x = i;
+                    y = u;
+                    var checks = 0;
+                    while (checks < 4)
+                    {
+                        if (gameboard[x, y] == cVar && gameboard[x, y] != "X")
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            cVar = gameboard[x, y];
+                            count = 1;
+                        }
+
+                        if (count == 4)
+                        {
+                            //Console.WriteLine("Dia: " + x + "-" + y);
+                            game.GameOver(cVar);
+                        }
+
+                        x++;
+                        y++;
+                        checks++;
+                    }
+                }
+                count = 0;
+            }
+
+            for (int u = 6; u > 2; u--)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    cVar = "X";
+                    x = i;
+                    y = u;
+                    var checks = 0;
+                    while (checks < 4)
+                    {
+                        if (gameboard[x, y] == cVar && gameboard[x, y] != "X")
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            cVar = gameboard[x, y];
+                            count = 1;
+                        }
+
+                        if (count == 4)
+                        {
+                            //Console.WriteLine("Dia: " + x + "-" + y);
+                            game.GameOver(cVar);
+                        }
+                        x++;
+                        y--;
+                        checks++;
+                    }
+                }
+                count = 0;
+            }
+
+            if (game.turn >= 43)
+            {
+                game.GameOver("draw");
+            }
+
+        }
+    }
+}
